bbf2d3c42bb99aacda17270d789f15a16fc021b9
siddolo <me@pasqualefiorillo.it>
2020-03-09 20:02:39 +0100

    Update notify.sh
    handle script_type (client-connect/client-disconnect), support send_smtp_message()

--- notify.sh
+++ notify.sh
@@ -4,9 +4,23 @@
     $time_ascii = getenv('time_ascii');
     $common_name = getenv('common_name');
     $untrusted_ip = getenv('untrusted_ip');
- 
+    $script_type = getenv('script_type');
+    $forceSMTP = TRUE;
+    
     $msg = "VPN Connection Event\n\n";
-    $msg .= $time_ascii . ' - ' . $common_name . ' connected from ' . $untrusted_ip . "\n";
- 
-    notify_all_remote($msg);
+    
+    if ($script_type === 'client-connect') {
+        $subject = "{$config['system']['hostname']}.{$config['system']['domain']} - VPN - $common_name ON";
+        $msg .= $time_ascii . ' - ' . $common_name . ' CONNECTED from ' . $untrusted_ip . "\n";
+    } else if ($script_type === 'client-disconnect') {
+        $subject = "{$config['system']['hostname']}.{$config['system']['domain']} - VPN - $common_name OFF";
+        $msg .= $time_ascii . ' - ' . $common_name . ' DISCONNECTED from ' . $untrusted_ip . "\n";
+    }
+
+    if ($forceSMTP === TRUE) {
+        send_smtp_message($msg, $subject);
+    } else {
+        //subject not supported here
+        notify_all_remote($msg);
+    }
 ?>
