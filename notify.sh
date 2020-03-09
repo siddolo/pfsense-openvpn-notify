5828ca67fb421f0a657ce8e237011c1c134dd9bb
siddolo <me@pasqualefiorillo.it>
2020-03-09 20:19:17 +0100

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -6,15 +6,15 @@
     $untrusted_ip = getenv('untrusted_ip');
     $script_type = getenv('script_type');
     $forceSMTP = TRUE;
-    
+
     $msg = "VPN Connection Event\n\n";
-    
+
     if ($script_type === 'client-connect') {
         $subject = "{$config['system']['hostname']}.{$config['system']['domain']} - VPN - $common_name ON";
         $msg .= $time_ascii . ' - ' . $common_name . ' CONNECTED from ' . $untrusted_ip . "\n";
     } else if ($script_type === 'client-disconnect') {
         $subject = "{$config['system']['hostname']}.{$config['system']['domain']} - VPN - $common_name OFF";
-        $msg .= $time_ascii . ' - ' . $common_name . ' DISCONNECTED from ' . $untrusted_ip . "\n";
+        $msg .= "{$time_ascii} - {$common_name} DISCONNECTED from {$untrusted_ip}\n";
     }
 
     if ($forceSMTP === TRUE) {
